a8c42789e27f1573ec4a1a5e6c8f31308aef5ed2
witemirlo <jacoboberdugo@gmail.com>
2024-11-13 11:54:43 +0100

    funtion to create a buffer from the config file whitout whitespace

--- src/split_config_file.cpp
+++ src/split_config_file.cpp
@@ -1,12 +1,38 @@
 #include <fstream>
+#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <vector>
 
+
+#include <iostream>
+
 std::string get_buffer(std::string const& path)
 {
-	std::string  buffer;
+	std::string  buffer, line, token;
 	std::fstream file(path.c_str());
+
+	if (!file.is_open())
+		return buffer;
+
+	// line puede ser un istringstream??
+	while (std::getline(file, line)) {
+		std::istringstream stream(line);
+		while (true) {
+			stream >> token;
+			if (token.empty())
+				break;
+
+			// std::cout << __FILE__ << ": " << __LINE__ << ": " << token << std::endl;
+
+			buffer += token;
+			buffer += ' ';
+			token.clear();
+		}
+		buffer[buffer.size() - 1] = '\n';
+	}
+
+	file.close();
 
 	return buffer;
 }
@@ -15,10 +41,14 @@ std::vector<std::string> split_config_file(std::string const& path)
 {
 	std::vector<std::string> container;
 	std::string              buffer;
-	// std::fstream             file(path.c_str());
-	// if (!file.is_open())
-	    // throw std::invalid_argument("");
+
 	buffer = get_buffer(path);
-	// file.close();
+	std::cout << buffer;
+
 	return container;
+}
+
+int main()
+{
+	split_config_file("config.toml");
 }
