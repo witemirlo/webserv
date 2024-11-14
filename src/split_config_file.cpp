a3d3d61e652e34f83bc75c036ade17784c9598ce
witemirlo <jacoboberdugo@gmail.com>
2024-11-14 13:46:39 +0100

    working in parsing the lines

--- src/split_config_file.cpp
+++ src/split_config_file.cpp
@@ -1,12 +1,14 @@
 #include <algorithm>
 #include <cctype>
 #include <cstddef>
+#include <endian.h>
 #include <fstream>
 #include <iterator>
 #include <stdexcept>
 #include <string>
 #include <sstream>
 #include <vector>
+#include <stack>
 
 
 #include <iostream> // DEBUG
@@ -80,37 +82,75 @@ std::string trim(std::string const& str)
 	std::string buffer;
 
 	start = 0;
-	while (str.size() && std::isspace(str[start]))
+	while (str.size() && (str[start] == ' ' || str[start] == '\t'))
 		start++;
 
 	end = str.size() - 1;
-	while (end && std::isspace(str[end]))
+	while (end && (str[end] == ' ' || str[end] == '\t' || buffer[end] == '\n'))
 		end--;
 	
-	buffer = str.substr(start, end);
+	buffer = str.substr(start, end + 1);
+	buffer = buffer.substr(0, buffer.find('#'));
+	buffer.push_back('\n');
 	return buffer;
 }
 
 int get_case(std::string const& str)
 {
 	std::string buffer = trim(str);
+	std::cerr << __FILE__ << ": " << __LINE__ << ": " << buffer;
 
 	// TODO: mirar las lineas que solo tienen comentarios
-	if (buffer.size() == 0)
+	if (buffer.size() == 0 || buffer[0] == '#' || buffer[0] == '\n')
 		return 2;
-
-	if (buffer.find('=') == buffer.rfind('='))
-		return 0;
 
 	if (buffer == "[server]\n")
 		return 1;
 
+	if (buffer.find('=') == buffer.rfind('='))
+		return 0;
+
 	return 3;
 }
 
-std::string get_line(std::istream& stream, std::string const& buffer)
+std::string get_line(std::istream& stream, std::string& buffer)
 {
 	std::string line;
+	std::stack<char> container;
+	std::size_t i;
+	bool open_brace;
+
+	i = 0;
+	open_brace = false;
+	do {
+		if (!buffer[i]) {
+			buffer.clear();
+			std::getline(stream, buffer);
+			i = 0;
+		}
+
+		switch (buffer[i]) {
+		case '[':
+			open_brace = true;
+			container.push('[');
+			line += buffer[i];
+			break;
+		case ']':
+			container.pop();
+			if (container.size() == 0)
+				open_brace = false;
+			line += buffer[i];
+			break;
+		// case '#':
+		// 	buffer.clear();
+		// 	break;
+		default:
+			line += buffer[i];
+			break;
+		}
+
+		i++;
+	} while (container.size() != 0 && open_brace);
 
 	return line;
 }
@@ -127,7 +167,6 @@ std::vector<std::string> get_raw_file(std::string const& path)
 		return container;
 
 	while (std::getline(file, buffer)) {
-		line.clear();
 		switch (get_case(buffer)) {
 		case 0:		
 			line = get_line(file, buffer);
@@ -140,7 +179,10 @@ std::vector<std::string> get_raw_file(std::string const& path)
 		default:		
 			throw std::invalid_argument("invalid argument");
 		}
-		if (line.size() )
+		// if (line.size() )
+		container.push_back(line);
+		buffer.clear();
+		line.clear();
 	}
 
 	file.close();
@@ -153,7 +195,7 @@ std::vector<std::string> split_config_file(std::string const& path)
 	std::string              buffer;
 
 	container = get_raw_file(path);
-	container = split_raw_file(container);
+	// container = split_raw_file(container);
 	// container = delete_quotes(container);
 
 	for (std::vector<std::string>::iterator it = container.begin(); it != container.end(); it++)
