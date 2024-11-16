2ae3160816da7a0d3d664dd58448c9fae70d01b0
witemirlo <jacoboberdugo@gmail.com>
2024-11-17 00:52:39 +0100

    cleaning code and rename split_config_file -> get_config_data

---
+++ include/get_config_data.hpp
@@ -0,0 +1,10 @@
+#ifndef _GET_CONFIG_DATA_HPP
+# define _GET_CONFIG_DATA_HPP
+
+#include <map>
+#include <string>
+#include <vector>
+
+std::vector<std::map<std::string, std::string> > get_config_data(std::string const&);
+
+#endif
