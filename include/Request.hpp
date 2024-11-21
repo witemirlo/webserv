114b66c8ffb6efca993b22db9fbaafc7a484410c
pablo-is-a-goblin <pablo.sacristan.pernia@gmail.com>
2024-11-21 15:22:22 +0100

    kinda of implemented Request class methods

--- include/Request.hpp
+++ include/Request.hpp
@@ -1,10 +1,12 @@
 #ifndef REQUEST_HPP
 # define REQUEST_HPP
+# define CRLF "\13\10" //TODO: he leido que algunos clientes solo hacen LF
 
-#include "colors.hpp"
+# include "colors.hpp"
 
 # include <string>
 # include <map>
+# include <iostream>
 
 enum request_status {
 	INIT,
@@ -20,8 +22,15 @@ private:
 	std::string _initial_line; //TODO: cambiar a method y url??
 	std::map<std::string, std::string> _headers;
 	std::string _body;
+	enum request_status _status;
 public:
-	enum request_status append_request(std::string & append);
+	enum request_status appendRequest(std::string & append);
+	void procHeader(std::string & raw, size_t index);
+
+//	GETTERs TODO: maybe its only for debug
+	std::string &getInitial(void);
+	std::string &getHeaders(void);
+	std::string &getBody(void);
 
 //	OCCF
 	Request();
@@ -29,5 +38,7 @@ public:
 	Request &operator=(const Request &other);
 	~Request();
 };
+
+std::ostream & operator<<(std::ostream & out, Request & req);
 
 #endif
--- src/Request.cpp
+++ src/Request.cpp
@@ -2,7 +2,83 @@
 
 #include <iostream>
 
-Request::Request(void)
+enum request_status Request::appendRequest(std::string & append)
+{
+	static std::string raw;
+
+	if (raw.size()) //TODO: que jacobo lo pase a ternaria
+		raw += append;
+	else
+		raw = append;
+
+	for (size_t ind = raw.find(CRLF); ind != std::string::npos; ind = raw.find(CRLF))
+	{
+		if (_status == INIT)
+		{
+			_initial_line = raw.substr(ind + 2);
+			_status == HEADERS;
+		}
+		else if (_status == HEADERS)
+			procHeader(raw, ind);
+		else if (_status == BODY)
+		{
+			_body = raw; //TODO: revisar fuerte, esto depende de headers y demas
+			_status = END;
+		}
+
+		raw.erase(0, ind + 2);
+	}
+
+	return (_status);
+}
+
+void Request::procHeader(std::string & raw, size_t index)
+{
+	std::string header = raw.substr(index);
+	size_t sep = header.find(":"); //TODO: errors y separador a lo peor es ": "
+	std::string key = header.substr(sep); //TODO: toupper o tulower
+	std::string value = header.substr(sep + 1, std::string::npos);
+
+	_headers[key] = value; //TODO: check for dups
+	if (raw[index + 2] == '\13' &&  raw[index + 3] == '\10') //TODO: podria ser mas limpio
+	{
+		_status = BODY; //TODO: si no hay body
+		raw.erase(2);
+	}
+}
+
+//	GETTERs TODO: maybe its only for debug
+
+std::string &Request::getInitial(void)
+{
+	return (_initial_line);
+}
+
+std::string &Request::getHeaders(void)
+{
+	std::string all_headers;
+
+	for (std::map<std::string, std::string>::iterator it = _headers.begin(); it != _headers.end(); it++)
+	{
+		all_headers += (it->first + ":" + it->second) + CRLF;
+	}
+	return (all_headers);
+}
+
+std::string &Request::getBody(void)
+{
+	return (_body);
+}
+
+std::ostream & operator<<(std::ostream & out, Request & req)
+{
+	out << req.getInitial() << req.getHeaders() << req.getBody();
+	return (out);
+}
+
+//	OCCF
+
+Request::Request(void) : _status(INIT)
 {
 #ifdef DEBUG
 	std::cout << GREEN "Request default constructor called" NC << std::endl;
