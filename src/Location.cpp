a4c3588ca2a1c4414e893aa750274ad4f52073e8
witemirlo <jacoboberdugo@gmail.com>
2024-12-03 20:04:24 +0100

    implementing methods

--- src/Location.cpp
+++ src/Location.cpp
@@ -123,3 +123,116 @@ std::string Location::getPathTo(std::string const& uri) const
 {
 	return (this->_root + uri);
 }
+
+std::string Location::getBody(std::string const& uri) const
+{
+	std::string path = getPathTo(uri); // TODO: este metodo deberia ser privado
+	struct stat sb;
+	// TODO: comprobar permisos de acceso
+	// TODO: comprobar si es un directorio -> comprobar autoindex
+	// TODO: comprobar si es un directorio -> comprobar index
+	if (access(path.c_str(), F_OK | R_OK) < 0) { // esto comprueba tambien directorios?
+		// TODO: que retorna?
+	}
+
+	stat(path.c_str(), &sb);
+	switch (sb.st_mode) {
+	case S_IFDIR: // directory file
+		// TODO: index
+		for (std::vector<std::string>::const_iterator it = this->_index.begin(); it != this->_index.end(); it++) {
+			if (access(path.c_str(), F_OK | R_OK) == 0) {
+				// TODO: el archivo existe
+				// TODO: deberia retornar (?)
+				return readFile(path);
+				// funcion para poner todo el archivo y retornarlo
+			}
+		}
+		// TODO: autoindex
+		// funcion para generar el autoindex
+		return autoIndex(path);
+		break;
+	
+	case S_IFREG: // regular file
+		// funcion para poner todo el archivo y retornarlo
+		return readFile(path);
+		break;
+	
+	default:
+		// TODO: error
+		// Error en funcion de errno
+		break;
+	}
+}
+
+std::string Location::readFile(std::string const& path) const
+{
+	std::fstream file;
+	std::string  buffer, final;
+
+	file.open(path);
+	if (!file.is_open()) // TODO: error?
+		return std::string("");
+	
+	while (getline(file, buffer))
+		final += buffer;
+
+	return final;
+}
+
+std::string Location::autoIndex(std::string const& path) const
+{
+	// TODO: y si termina en '/'?
+	struct dirent*       file;
+	DIR*                 directory;
+	struct stat          sb;
+	std::stringstream    buffer;
+	char                 date[512];
+
+	directory = opendir(path.c_str());
+	if (directory == NULL) {
+		// TODO
+	}
+
+	buffer <<  "<html>\n"
+	       << "<head><title>Index of " << path << "</title></head>\n"
+	       << "<body>\n"
+	       << "<h1>Index of " << path << "</h1><hr><pre><a href=\"../\">../</a>\n";
+
+	while (true) {
+		std::memset(&sb, 0, sizeof(struct stat));
+		std::memset(&date, 0, 512);
+		
+		file = readdir(directory);
+
+		if (file == NULL) {
+			// TODO: control de errores
+			break;
+		}
+
+		if (file->d_name[0] == '.')
+			continue;
+
+		stat(file->d_name, &sb);
+		if (file == NULL)
+			break;
+
+		std::strftime(date, 512, "%d-%B-%Y %H:%M", std::gmtime(&sb.st_mtim.tv_sec));
+
+		buffer << "<a href=\""
+		       << file->d_name // NOTE: cambiar, name
+		       << "\">"
+		       << std::left << std::setw(80) << file->d_name // NOTE: cambiar, name
+		       << "</a>"
+		       << std::setw(1) << date // NOTE: cambiar, ultima modificacion
+		       << "\t"
+		       << sb.st_size // NOTE: cambiar, size
+		       << "\n";
+	}
+
+	buffer << "</pre><hr></body>\n"
+	       << "</html>\n";
+
+	closedir(directory);
+
+	return buffer.str();
+}
