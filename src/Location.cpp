79a32147e4d864776f36c78eb3dd06285e68e805
witemirlo <jacoboberdugo@gmail.com>
2024-12-04 14:03:17 +0100

    add comments to methods

--- src/Location.cpp
+++ src/Location.cpp
@@ -124,24 +124,41 @@ bool Location::operator<=(const Location & other) const
 	return (this->_deepness <= other._deepness);
 }
 
+/**
+ * Generates the full path to a file
+ *
+ * @param uri uri to the file
+ * @return string with the full path to the file
+ */
 std::string Location::getPathTo(std::string const& uri) const
 {
 	return (this->_root + uri);
 }
 
+/**
+ * Generates the body of the response to a GET method
+ *
+ * @param uri uri to the file
+ * @return string with the body of the response
+ */
 std::string Location::getBody(std::string const& uri) const
 {
-	std::string path = getPathTo(uri); // TODO: este metodo deberia ser privado
-	struct stat sb;
+	std::string path;
+	struct stat file_info;
+
 	// TODO: comprobar permisos de acceso
 	// TODO: comprobar si es un directorio -> comprobar autoindex
 	// TODO: comprobar si es un directorio -> comprobar index
-	if (access(path.c_str(), F_OK | R_OK) < 0) { // esto comprueba tambien directorios?
+	path = getPathTo(uri);
+	if (access(path.c_str(), R_OK) < 0) {
 		// TODO: que retorna?
 	}
 
-	stat(path.c_str(), &sb);
-	switch (sb.st_mode) {
+	if (stat(path.c_str(), &file_info) < 0) {
+		// TODO: que retorna?
+	}
+
+	switch (file_info.st_mode) {
 	case S_IFDIR: // directory file
 		for (std::vector<std::string>::const_iterator it = this->_index.begin(); it != this->_index.end(); it++) {
 			if (access(path.c_str(), R_OK) == 0) {
@@ -168,6 +185,12 @@ std::string Location::getBody(std::string const& uri) const
 	}
 }
 
+/**
+ * Generates string with all the content in a given file
+ *
+ * @param path path to the file to read
+ * @return string with all the content in the file
+ */
 std::string Location::readFile(std::string const& path) const
 {
 	std::fstream file;
@@ -186,13 +209,19 @@ std::string Location::readFile(std::string const& path) const
 	return final;
 }
 
+/**
+ * Generates html with the files in a given directory
+ *
+ * @param path path to the directory
+ * @return string with a list of files in the directory in html
+ */
 std::string Location::autoIndex(std::string const& path) const
 {
 	// TODO: y si termina en '/'?
 	std::stringstream    buffer;
 	DIR*                 directory;
 	struct dirent*       file;
-	struct stat          file_status;
+	struct stat          file_info;
 	char                 date[512];
 
 	directory = opendir(path.c_str());
@@ -206,7 +235,7 @@ std::string Location::autoIndex(std::string const& path) const
 	       << "<h1>Index of " << path << "</h1><hr><pre><a href=\"../\">../</a>\n";
 
 	while (true) {
-		std::memset(&file_status, 0, sizeof(struct stat));
+		std::memset(&file_info, 0, sizeof(struct stat));
 		std::memset(&date, 0, 512);
 		
 		file = readdir(directory);
@@ -219,15 +248,15 @@ std::string Location::autoIndex(std::string const& path) const
 		if (file->d_name[0] == '.')
 			continue;
 
-		stat(file->d_name, &file_status);
+		stat(file->d_name, &file_info);
 		if (file == NULL)
 			break;
 
-		std::strftime(date, 512, "%d-%B-%Y %H:%M", std::gmtime(&file_status.st_mtim.tv_sec));
+		std::strftime(date, 512, "%d-%B-%Y %H:%M", std::gmtime(&file_info.st_mtim.tv_sec));
 
 		buffer << "<a href=\"" << file->d_name << "\">"                        // NOTE: link to the file
 		       << std::left << std::setw(80) << file->d_name << "</a>"         // NOTE: text of the link
-		       << std::setw(1) << date << "\t" << file_status.st_size << "\n"; // NOTE: file info
+		       << std::setw(1) << date << "\t" << file_info.st_size << "\n"; // NOTE: file info
 	}
 
 	buffer << "</pre><hr></body>\n"
