f4603e55352b11eeb37a52ea1b4d61fbcf8761a4
witemirlo <jacoboberdugo@gmail.com>
2024-12-11 14:40:20 +0100

    document code and function to get the content-type header param from the file type

--- include/Location.hpp
+++ include/Location.hpp
@@ -3,25 +3,25 @@
 
 # include "Server.hpp"
 
-class Location : public Server
-{
+class Location : public Server {
 private:
 	unsigned int _deepness;
 	static const std::string prohibited_rules[];
-	static const std::map<std::string, std::string> _file_types[];
+	static const std::pair<std::string, std::string> _file_types[];
 
 	std::map<std::string, std::string> getCgiHeaders(std::string const& body) const;
 	std::string getFileType(std::string const& file) const;
 	std::string getBody(std::string const& uri) const;
 	std::string getBodyError(int status_code) const;
 	std::string getGmtTime(void) const;
+	std::string getContentType(std::string const& uri) const;
 	std::string getPathTo(std::string const& uri) const;
 	std::string readFile(std::string const& path) const;
 	std::string autoIndex(std::string const& path) const;
 
 	int getStatusCode(void) const;
 
-	std::string getHeaders(std::string const& body) const;
+	std::string getHeaders(std::string const& body, std::string const& uri) const;
 
 public:
 	Location();
